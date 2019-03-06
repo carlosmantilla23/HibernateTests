f096ec63744afc365f190670d58a37872a1a8ba3
carlosmantilla23 <carlos.mantilla.jaimes@gmail.com>
2019-03-05 22:55:07 -0500

    Creo Bean SocialMedia

---
+++ src/main/java/com/carlosmantilla/HibernatePlazti/model/SocialMedia.java
@@ -0,0 +1,40 @@
+package com.carlosmantilla.HibernatePlazti.model;
+
+import java.io.Serializable;
+
+public class SocialMedia implements Serializable {
+	
+	private Long idSocialMedia;
+	private String name;
+	private String icon;
+	
+	public Long getIdSocialMedia() {
+		return idSocialMedia;
+	}
+	public void setIdSocialMedia(Long idSocialMedia) {
+		this.idSocialMedia = idSocialMedia;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getIcon() {
+		return icon;
+	}
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+	public SocialMedia() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public SocialMedia(String name, String icon) {
+		super();
+		this.name = name;
+		this.icon = icon;
+	}
+	
+
+}
