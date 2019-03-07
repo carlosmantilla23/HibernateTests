56a4565110ec649aecb6a221f482eefaa11d90c7
carlosmantilla23 <carlos.mantilla.jaimes@gmail.com>
2019-03-06 22:24:58 -0500

    Abro conexión a BD

--- src/main/java/com/carlosmantilla/HibernatePlazti/App.java
+++ src/main/java/com/carlosmantilla/HibernatePlazti/App.java
@@ -1,13 +1,24 @@
 package com.carlosmantilla.HibernatePlazti;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.carlosmantilla.HibernatePlazti.model.Teacher;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		// Abrimos conexiòn:
+		SessionFactory sessionFactory;
+		Configuration configuration = new Configuration();
+		configuration.configure();
+		sessionFactory = configuration.buildSessionFactory();
+		Session session = sessionFactory.openSession();
+
+	}
 }
